57b31c8c0019c8836bad9e6399566427dcf907fd
veroeb <vecheza@gmail.com>
2024-08-30 00:17:55 -0300

    Added new drag and drop functionality

--- components/kanbanColumns.js
+++ components/kanbanColumns.js
@@ -20,15 +20,18 @@ export class TaskColumns {
     createColumn(status) {
         const columnDiv = document.createElement("div");
         columnDiv.classList.add("column", "is-one-fifth");
+        columnDiv.dataset.status = status;
         columnDiv.innerHTML = `
             <div class="column">
                 <header class="card-header">
                     <p class="card-header-title">${status}</p>
                 </header>
-                <div class="card-content" id="column-${status.replace(/\s+/g, "")}">
+                <div class="card-content" id="column-${status.replace(/\s+/g, "")}" ondrop="event.stopPropagation(); return false;">
                 </div>
             </div>
         `;
+        columnDiv.addEventListener("dragover", this.allowDrop);
+        columnDiv.addEventListener("drop", (event) => this.drop(event));
         return columnDiv;
     }
 
@@ -48,9 +51,10 @@ export class TaskColumns {
 
     createTaskElement(task) {
         const taskDiv = document.createElement("div");
-        taskDiv.classList.add("card", "mb-3", "task-card");
+        taskDiv.classList.add("card", "mb-3", "task-card", `task-${task.status.replace(/\s+/g, "").toLowerCase()}`);
         taskDiv.dataset.taskId = task.id;
-        taskDiv.style.cursor = "pointer";
+        taskDiv.draggable = true;
+        taskDiv.ondragstart = (event) => this.drag(event);
         taskDiv.innerHTML = `
             <div class="card-content">
                 <p class="title is-5">${task.title}</p>
@@ -58,12 +62,31 @@ export class TaskColumns {
                 <p class="subtitle is-6">Asignado a: ${task.assigned}</p>
             </div>
         `;
-    
+
         taskDiv.addEventListener("click", () => {
             const taskToEdit = this.tasks.find(t => t.id === task.id);
             this.modal.openModal("Editar Tarea", taskToEdit); 
         });
-    
+
         return taskDiv;
+    }
+
+    drag(event) {
+        event.dataTransfer.setData("text", event.target.dataset.taskId);
+    }
+
+    allowDrop(event) {
+        event.preventDefault();
+    }
+
+    drop(event) {
+        event.preventDefault();
+        const taskId = event.dataTransfer.getData("text");
+        const newStatus = event.currentTarget.dataset.status;
+
+        const taskToUpdate = this.tasks.find(task => task.id === taskId);
+        taskToUpdate.status = newStatus;
+
+        this.loadTasksToColumns();
     }
 }
